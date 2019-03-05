74edc4465ff3fe2e86873b4487623f53c68811b3
Jungseung Yang <xyloon@gmail.com>
2019-03-05 18:12:50 +0900

    numeric format changer from string to decimal

--- kexr/kexr.py
+++ kexr/kexr.py
@@ -1,4 +1,5 @@
 import re
+from decimal import Decimal
 
 import requests
 
@@ -6,7 +7,8 @@ float_pattern = re.compile(r"([^0-9., ]+)")
 
 
 def remove_comma_in_float_string(i_str):
-    return None if tuple(float_pattern.findall(i_str)) else float(i_str.replace(",", "").strip())
+    'converting number string to Decimal'
+    return None if tuple(float_pattern.findall(i_str)) else Decimal(i_str.replace(",", "").strip())
 
 
 def get_request_url(key, search_date):
@@ -21,6 +23,27 @@ def request_exchange(key, search_date):
         raise Exception("Error repsonse " + str(resp))
     else:
         return resp.json()
+
+
+def resp_dict_converter(i_dict_list):
+    'converting number string to Decimal in dict'
+
+    'sample input'
+    # {'bkpr': '290',
+    #       'cur_nm': '아랍에미리트 디르함',
+    #       'cur_unit': 'AED',
+    #       'deal_bas_r': '290.95',
+    #       'kftc_bkpr': '290',
+    #       'kftc_deal_bas_r': '290.95',
+    #       'result': 1,
+    #       'ten_dd_efee_r': '0',
+    #       'ttb': '288.04',
+    #       'tts': '293.85',
+    #       'yy_efee_r': '0'},
+    key_to_process = ['bkpr', 'deal_bas_r', 'kftc_deal_bas_r', 'ten_dd_efee_r', 'ttb', 'tts', 'yy_efee_r']
+    return [dict(one_line if one_line[0] not in key_to_process else (one_line[0], remove_comma_in_float_string(one_line[1]) ) for one_line in one_item.items()) for one_item in i_dict_list]
+
+
 
 # todo collect data for one year
 # date, currency, ratio is format
---
+++ test/test_resp_dict_converter.py
@@ -0,0 +1,23 @@
+import json
+
+from kexr.kexr import resp_dict_converter
+from test.const import resp_json_exch
+
+
+def test_resp_dict_converter():
+    input = json.loads(resp_json_exch)
+    # one item after processed
+    # [{'bkpr': Decimal('290'),
+    #   'cur_nm': '아랍에미리트 디르함',
+    #   'cur_unit': 'AED',
+    #   'deal_bas_r': Decimal('290.95'),
+    #   'kftc_bkpr': '290',
+    #   'kftc_deal_bas_r': Decimal('290.95'),
+    #   'result': 1,
+    #   'ten_dd_efee_r': Decimal('0'),
+    #   'ttb': Decimal('288.04'),
+    #   'tts': Decimal('293.85'),
+    #   'yy_efee_r': Decimal('0')}]
+    #
+    assert float(
+        [one_item['tts'] for one_item in resp_dict_converter(input) if one_item['cur_unit'] == 'AED'][0]) == 293.85
